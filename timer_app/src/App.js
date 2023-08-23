d0491763e9ccca32682fc4aa3b341b1ee0035722
georgeussher <gussher@icloud.com>
2023-08-23 14:53:45 +0100

    addItem logic added

--- timer_app/src/App.js
+++ timer_app/src/App.js
@@ -4,17 +4,17 @@ import Item from './components/Item';
 import AddItem from './components/AddItem';
 
 function App() {
-	//state: itemName, countdown
-	//passes down itemName and countdown to Item component
-	//passes down setItemName and setCountdown to AddItem component
-	//renders Item and AddItem
-	let [itemName, setItemName] = useState('');
-	return (
-		<div>
-			<AddItem setItemName={setItemName} />
-			<Item itemName={itemName} />
-		</div>
-	);
+    //state: itemName, countdown
+    //passes down itemName and countdown to Item component
+    //passes down setItemName and setCountdown to AddItem component
+    //renders Item and AddItem
+    let [itemName, setItemName] = useState('');
+    return (
+        <div>
+            <AddItem setItemName={setItemName} />
+            <Item itemName={itemName} />
+        </div>
+    );
 }
 
 export default App;

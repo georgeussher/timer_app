2125bbcdad940789f101faf8e5aa7edd1d7aae9a
georgeussher <gussher@icloud.com>
2023-08-24 18:29:02 +0100

    plan adjusted

--- timer_app/src/App.js
+++ timer_app/src/App.js
@@ -1,13 +1,13 @@
-//import './App.css'
+import './App.css'
 import React, { useState } from 'react';
 import Item from './components/Item';
 import AddItem from './components/AddItem';
 
-function App() {
+export default function App() {
     //state: itemName, countdown
     //passes down itemName and countdown to Item component
     //passes down setItemName and setCountdown to AddItem component
-    //renders Item and AddItem
+    //renders Item and countdown
     let [itemName, setItemName] = useState('');
     return (
         <div>
@@ -17,7 +17,6 @@ function App() {
     );
 }
 
-export default App;
 
 //next steps: set up react router so when add button is clicked user navigates to add-item page
 //add 'Add' button that navigates back to home and renders Item component as a list

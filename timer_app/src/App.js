4a7632a0bd11b1a286a49f481a3bbe74e99a2b36
georgeussher <gussher@icloud.com>
2023-08-27 14:35:24 +0100

    added to nextsteps

--- timer_app/src/App.js
+++ timer_app/src/App.js
@@ -5,8 +5,10 @@ import Homepage from './pages/homepage';
 import AddItem from './pages/addItem';
 
 function App() {
+    //STATE:
 	let [itemName, setItemName] = useState('');
     let [items, setItems] = useState([])
+
 	return (
 
 		<Routes>
@@ -42,3 +44,10 @@ export default function Root() {
 }
 //next steps: set up react router so when add button is clicked user navigates to add-item page✅
 //add 'Add' button that navigates back to home and renders Item component as a list✅
+//add timer functionality to Item component
+//render countdown timer in homepage according to parameters set in Item (state?)
+//order list according to timer length (shortest at the top)
+//render totat time at top of homepage
+//when timer ends make sound
+//be able to delete items from list
+//stop timer when item is deleted
--- timer_app/src/pages/addItem.js
+++ timer_app/src/pages/addItem.js
@@ -5,18 +5,21 @@ export default function AddItem({ setItemName, items, setItems, itemName }) {
 	//contains input field for item name whose value is passsed into setItemName
 	//contains timer wheel whose value is assigned to setCountdown
 
+    //updates itemName state when input field is changed
 	const handleInputChange = (event) => {
 		setItemName(event.target.value);
 	};
 
-    const handleClick = () => {
-            setItems([...items, itemName]);
-            setItemName('');
-    }
+    //adds item to items array and clears input field
+	const handleClick = () => {
+		setItems([...items, itemName]);
+		setItemName('');
+	};
 
-    const isTextEmpty = (text) => {
-        return text.trim() === '';
-    }
+    //returns true if input field is empty and false if not
+	const isTextEmpty = (text) => {
+		return text.trim() === '';
+	};
 
 	return (
 		<>
@@ -24,7 +27,9 @@ export default function AddItem({ setItemName, items, setItems, itemName }) {
 				<input type='text' onChange={handleInputChange} />
 			</div>
 			<Link to='/' aria-label='add item and switch to homepage'>
-				<button onClick={handleClick} disabled={isTextEmpty}>Add</button>
+				<button onClick={handleClick} disabled={isTextEmpty}>
+					Add
+				</button>
 			</Link>
 		</>
 	);

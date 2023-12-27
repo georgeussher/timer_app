530616f2a0886ba5aee1dffacf899b636137106c
georgeussher <gussher@icloud.com>
2023-12-27 16:23:29 +0000

    created targetDate from hours, mins & secs

--- timer_app/src/components/Countdown.js
+++ timer_app/src/components/Countdown.js
@@ -1,9 +1,22 @@
 //Countdown timer logic rendered inside Item component
-
+import setInterval from 'react'
 //PLAN: save time as state variable to be changed in the add-item component
 
 export default function Countdown({ hours, minutes, seconds }) {
+	//convert hours, minutes and seconds from string to integer
+	const convertedHours = parseInt(hours, 10);
+	const convertedMinutes = parseInt(minutes, 10);
+	const convertedSeconds = parseInt(seconds, 10);
+
 	//COUNTDOWN LOGIC HERE
+		const currentDate = new Date();
+		//convert hours, minutes and seconds into milliseconds and combine into one number
+		const durationInMilliseconds = (convertedHours * 3600 + convertedMinutes * 60 + convertedSeconds) * 1000;
+		const targetDate = new Date(currentDate.getTime() + durationInMilliseconds)
+
+	
+
+
 	//on routing to homepage, timer function called, taking in hours, minutes and seconds as arguments
 	return (
 		<div>
